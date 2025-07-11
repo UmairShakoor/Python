316cc30a6b8caa367d5cad42de31395729252f47
Umair Shakoor <umairshakoor.pro@gmail.com>
2025-07-11 10:20:11 +0000

    Added chapters and projects in Python repo from local machine

---
+++ 08. Input Validation/02. integer_validation.py
@@ -0,0 +1,18 @@
+# We can convert string's input to any other dataType , Most common conversion is to integer.
+
+# age = int(input('Enter your age: '))
+# Your program will crash if user enters abc..
+
+# To avoid crash we have to verify if the input is integer before converting input() to integer
+
+age = input('Enter you age: ')
+
+# .isdigit() checks if string is made of integers only . It returns False even if string contains a floating point number
+# .isdigit() is very strict and returns False even string contains a space.
+if age.isdigit():
+    age = int(age)
+    print(f'Thanks, Age Recorded: {age}')
+
+else:
+    print('Invalid Age, Age must be an integer')
+
